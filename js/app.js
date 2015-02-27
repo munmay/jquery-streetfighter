a07584e5c45fd654b68c915e06f87ca5e58a41a6
Mun May Tee <munmay.g@gmail.com>
2015-02-27 12:50:04 -0800

    more actions

--- js/app.js
+++ js/app.js
@@ -7,6 +7,17 @@ $(document).ready(function() {
   .mouseleave(function(){
   	$('.ryu-ready').hide();
   	$('.ryu-still').show();
-  });
+  })
+
+  .mousedown(function(){
+  	$('.ryu-ready').hide();
+  	$('.ryu-throwing').show();
+  	$('.hadouken').show();
+  })
+
+  .mouseup(function(){
+  	$('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  })
 });
 
