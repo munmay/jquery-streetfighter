f21be4fb1e83a082407b61aa038e7367f4bf7692
Mun May Tee <munmay.g@gmail.com>
2015-02-27 21:12:31 -0800

    added cool

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
+
+$('body').keydown(function(e){
+  		if(e.keyCode == 88){
+  		   $('.ryu-still').hide();
+  		   $('.ryu-ready').hide();
+  		   $('.ryu-cool').show();
+  	     }
+  	})
+
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-ready').show();
   })
 
   .mouseleave(function(){
   	$('.ryu-ready').hide();
   	$('.ryu-still').show();
+  	$('.ryu-cool').hide();
   })
 
+
+
   .mousedown(function(){
+  	playHadouken();
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('.hadouken').show();
+  	$('.hadouken').finish().show().animate(
+  		{'left': '500px'}, 500,
+  		function(){
+  			$(this).hide();
+  			$(this).css('left','0px');
+  		});
   })
 
   .mouseup(function(){
@@ -21,3 +40,8 @@ $(document).ready(function() {
   })
 });
 
+function playHadouken(){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
